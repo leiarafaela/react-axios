270622545d1cf8881e8d0a6c1e5aaada8395ab1b
rafaela <rafaela.augusto@somei.com.vc>
2022-10-22 19:41:16 -0300

    add home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React, { useEffect, useState } from "react";
-import api from "./services/api";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css';
+import Home from './Home';
+import Usuario from "./callApi";
 
-export default function App() {
-  const [users, setUser] = useState();
-
-  useEffect(() => {
-    api
-      .get("/usuarios")
-      .then(({data}) => {setUser(data);
-      console.log(users)
-      });
-
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-
+function App() {
   return (
     <div>
-          {JSON.stringify(users)}
-          
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/usuarios" exact element={<Usuario />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
+
+export default App;
